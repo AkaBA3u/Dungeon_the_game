3cf156473865c530a5b8615ed639aa5e851205e4
AkaBA3u <arseniyshalashov0402@yandex.ru>
2022-12-20 17:20:57 +0300

    Empty file

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 1280, 720
+    window = pygame.display.set_mode(size)
+    fps = 60
+    clock = pygame.time.Clock()
+
+    running = True
+
+    while running:
+        window.fill('black')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            if event.type == pygame.MOUSEBUTTONUP:
+                pass
+            if event.type == pygame.MOUSEMOTION:
+                pass
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
